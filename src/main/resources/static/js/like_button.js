b3167d5083e3e8d7b286873898d522bd24748b87
Keyhoh <iheyhoh@gmail.com>
2019-05-06 02:26:11 +0900

    add react and react bootstrap

---
+++ src/main/resources/static/js/like_button.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return e(
+                ReactBootstrap.Alert,
+                {
+                    variant: 'primary',
+                },
+                'This is a primary alert-check it out!',
+            );
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
